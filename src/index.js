88767f95c4d7b0ca4d07bc3ac00229e154e3ad18
Anatoly Belobrovik <anatoliybelobrovik@gmail.com>
2018-02-16 17:48:54 +0300

    Set up apollo server

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 import logger from 'koa-logger';
+import Router from 'koa-router';
 import dotenv from 'dotenv';
+import { graphqlKoa, graphiqlKoa } from 'apollo-server-koa';
+import { makeExecutableSchema } from 'graphql-tools';
 
 import db from './config/db';
+import typeDefs from './models/schemas';
+import resolvers from './models/resolvers';
 
 dotenv.config();
 
 const app = new Koa();
 const port = process.env.PORT || 8080;
+const router = new Router();
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+router.post('/graphql', graphqlKoa({ schema }));
+router.get('/graphql', graphqlKoa({ schema }));
+router.get('/graphiql', graphiqlKoa({ endpointURL: '/graphql' }));
 
 db();
 
 app.use(bodyParser());
+app.use(router.routes());
+app.use(router.allowedMethods());
 app.use(logger());
 
 app.listen(port);
--- src/models/schemas/index.js
+++ src/models/schemas/index.js
@@ -1,14 +1,14 @@
 export default `
-type Tweet {
-  _id: String,
-  text: String
-}
+  type Tweet {
+    _id: String
+    text: String
+  }
 
-type Query {
-  getTweets: [Tweet]
-}
+  type Query {
+    getTweets: [Tweet]
+  }
 
-schema {
-  query: Query
-}
+  schema {
+    query: Query
+  }
 `;
